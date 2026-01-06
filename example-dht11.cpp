fa1b881311db8254dc891dd2f03d7177286b9502
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-06 09:47:12 +0700

    Create example-dht11.cpp

---
+++ example-dht11.cpp
@@ -0,0 +1,26 @@
+#include "dht.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+void dht_11(void *arg)
+{
+    static const char *TAG = "DHT11";
+    while (1)
+    {
+        if (!dht11::getData(GPIO_NUM_4))
+        {
+            ESP_LOGE(TAG, "Error");
+        }
+
+        ESP_LOGI(TAG, "Temp: %.2f", dht11::getTEMP());
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+extern "C" void app_main()
+{
+
+    dht11::init(GPIO_NUM_4);
+    xTaskCreatePinnedToCore(dht_11, "get temp and hum", 2048, NULL, 5, NULL, 1);
+}
